3ebda36f1bc155b5f173d2d45183596bc29c3194
free3 <free3xm@gmail.com>
2019-11-07 16:28:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
-const regExp = [[/foo/gi, "bar"],[/cat/gi,"dog"],
-                [/helo/gi,"hello"], [/heldp/gi, "help"]];
+const regExp = [[/\bfoo\b/gi, "bar"],[/\bcat\b/gi,"dog"],
+                [/\bhelo\b/gi,"hello"], [/\bheldp\b/gi, "help"]];
 function textRaplcer(event, input){
   let text;
+
   if(input){
+    let posStart = event.target.selectionStart,
+        posEnd = event.target.selectionEnd;
     text = event.target.value.slice();
-    regExp.forEach((e, i) => text = text.replace(e[0], e[1]));
-    event.target.value = text;
+
+    if(checkWord(text)){
+      regExp.forEach((e, i) => text = text.replace(e[0], e[1]));
+      event.target.value = text;
+      event.target.setSelectionRange(posStart, posEnd);
+    }
+    
   } else {
-    text = event.target.textContent.slice();
-    regExp.forEach((e, i) => text = text.replace(e[0], e[1]));
-    event.target.textContent = text;
+      let sel = document.getSelection(),
+        range = document.createRange(),
+        pos = sel.getRangeAt(0).startOffset,
+        elemFocus = sel.focusNode;
+      text = elemFocus.textContent.slice();
+
+      if(checkWord(text)){
+        regExp.forEach((e, i) => text = text.replace(e[0], e[1]));
+        elemFocus.textContent = text;
+        range.setStart(elemFocus, pos);
+        range.setEnd(elemFocus, pos);
+        sel.removeAllRanges();
+        sel.addRange(range);
+      }
+  }
+
+  function checkWord(value){
+    if(regExp.some(e => value.match(e[0]))) return true;
+    return false;
   }
 }
-
-document.body.addEventListener("keydown", event => {
+document.body.addEventListener("keyup", event => {
   if(event.code == "Space"){
     if(event.target.tagName == "INPUT" && event.target.type == "text"){
       textRaplcer(event, true);
     }
-    else if(event.target.getAttribute("contenteditable")){
+    else if(event.target.hasAttribute("contenteditable")){
       textRaplcer(event);
     }
   }
-});
+
+}, false);
